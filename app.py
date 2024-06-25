db5372e484d5e7d8132c8b56047ca0e54a497934
isimane <simaneid05@gmail.com>
2024-06-24 18:02:39 -0700

    edited my code

--- app.py
+++ app.py
@@ -25,24 +25,37 @@ def home():
         return render_template("productstable.html", products=data)
     
 
-@app.route("/add_product", methods=["GET", "POST"]) 
+@app.route("/add_product", methods=['GET', 'POST']) 
 def add_product():
-    if request.method == "POST":
-        id = request.form["id"]
-        name = request.form["name"]
-        description = request.form["description"]
-        price = request.form["price"]
-        image = request.form["image"]
-        quantity = request.form["quantity"]
+    if request.method == 'POST':
+        name = request.form["ProductName"]
+        image = request.files["ProductImage"]
+        description = request.form["ProductDescription"]
+        price = request.form["ProductPrice"]
+        quantity = request.form["ProductQuantity"]
         with sqlite3.connect("db.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO products (name, description, price, image, quantity) VALUES (?, ?, ?, ?, ?)",(name, description, price, image,quantity))
+            cur.execute("INSERT INTO products(name, image, description, price, quantity) values (?,?,?,?,?)",(name,image,description,price,quantity))
             con.commit()
-            flash('Product added','success')
-            return redirect(url_for("home"))
+            # flash('Product added','success')
+        return redirect(url_for("home"))
     return render_template("addProduct.html")
 
-
+@app.route("/products") 
+def products():
+     with sqlite3.connect("db.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("""
+            SELECT prod.*, categ.name AS category_name
+            FROM products prod
+            JOIN categories categ ON prod.category_id = categ.id
+            ORDER BY RANDOM()
+        """)
+       
+        data = cur.fetchall()
+        return render_template("shop.html", products=data)
+        
 
 if __name__ == "__main__":
     # db.create_all
---
+++ shop.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for,request,redirect,url_for,flash,session
+# from flask_sqlalchemy import SQLAlchemy'
+import sqlite3
+
+# import os
+
+app=Flask(__name__)
+
+
+
+if __name__ == "__main__":
+    # db.create_all
+    app.run(debug=True)
