9ba9e4f5997d0b77dd8be840e8dec370f19ebd96
isimane <simaneid05@gmail.com>
2024-06-28 00:08:34 +0100

    made my code better also i added some thing for the edit in my CRUD

--- app.py
+++ app.py
@@ -65,11 +65,12 @@ def add_product():
     return render_template("addProduct.html")
 
 
-@app.route("/<string:id>/edit_product", methods=['GET','POST'])
+@app.route("/edit_product/<int:id>", methods=['GET','POST'])
 def edit_product(id):
+    product_id = int(id)
     with sqlite3.connect("db.db") as con:
       cur = con.cursor()
-      cur.execute("SELECT * FROM products WHERE id =?", (id[0]))
+      cur.execute("SELECT * FROM products WHERE id =?", (id,))
       product=cur.fetchone()
     if request.method == "POST":
         name = request.form["ProductName"]
@@ -78,14 +79,55 @@ def edit_product(id):
         quantity = request.form["ProductQuantity"]
         category_id = request.form["ProductCategory"]
         image = request.files["ProductImage"]
+        if image:
+            image.save(os.path.join(
+                app.config['UPLOAD_DIRECTORY'],
+                secure_filename(image.filename)
+            ))
+            image_filename = image.filename
+        else :
+            image_filename = product[1]
         cur.execute("UPDATE products SET name =?, image=?, description=?, price=?, quantity=?, category_id=? WHERE id=?",
-                        (name, image.filename, description, price, quantity, category_id, id))
+                        (name, image.filename if image else product[1], description, price, quantity, category_id, id))
         con.commit()
         flash('Product updated', 'success')
         return redirect(url_for("home"))
     else:
-        categories = cur.execute("SELECT * FROM categories").fetchall()
-        return render_template("editProduct.html", product=product, categories=categories)
-    
+            categories = cur.execute("SELECT * FROM categories").fetchall()
+            return render_template("editProduct.html",product_id = product_id, product=product, categories=categories)
+ 
+@app.route("/shop")
+def shop():   
+    category = request.args.get("category")
+    with sqlite3.connect("db.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        sql = """
+            SELECT prod.*, categ.name AS category_name
+            FROM products prod
+            JOIN categories categ ON prod.category_id = categ.id
+        """
+        if category:
+            sql += " WHERE categ.id = ?"
+            
+        sql += " ORDER BY RANDOM()"
+        
+        if category:
+            cur.execute(sql, (int(category),))
+        else:
+            cur.execute(sql)
+            
+        data = cur.fetchall()
+            
+        if category:
+         # get category name from db using the id
+           categoryname = "SELECT name FROM categories WHERE id = ?"
+           cur.execute(categoryname, (int(category),))
+           category_name = cur.fetchone()[0]
+           category = category_name
+
+        return render_template("shop.html", products=data, category=category) 
+ 
+   
 if __name__ == "__main__":
     app.run(debug=True)
